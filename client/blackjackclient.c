2ef209b29f2abf5e8504e2247efabeff4e1794f9
matteo-moro <moro.matteorosario@gmail.com>
2025-07-08 11:54:46 +0200

    Update blackjackclient.c

--- client/blackjackclient.c
+++ client/blackjackclient.c
@@ -100,6 +100,9 @@ int client_connection_init()
 }
     
 // gestisce il processo di registrazione e di login con il server
+// username e password sono le stringhe per username e password da usare
+// mode può essere LOGIN_MODE o REGISTER_MODE e specifica l'operazione da eseguire
+// ritorna 0 in caso di successo, 1 in caso di errore
 int server_login(char username[], char password[], MODE mode)
 {
     init_result = client_connection_init();
@@ -156,6 +159,7 @@ int server_login(char username[], char password[], MODE mode)
     if (mode == LOGIN_MODE)
     {
 
+        // manda query di login al server
         strcpy(qbuffer, "Login");
 
         iResult = send(ConnectSocket, qbuffer, DEFAULT_BUFLEN, 0);
@@ -167,6 +171,7 @@ int server_login(char username[], char password[], MODE mode)
             return 1;
         }
 
+        // ricevi risultato del login e restituisci valore corretto
         iResult = recv(ConnectSocket, recvbuf, DEFAULT_BUFLEN, 0);
 
         if (strcmp(recvbuf, "logok") == 0)
@@ -185,5 +190,145 @@ int server_login(char username[], char password[], MODE mode)
             WSACleanup();
             return LOGIN_SUCCESS;
         }
+        else if (strcmp(recvbuf, "logfail") == 0)
+        {
+            printf("login failed\n");
+
+            iResult = shutdown(ConnectSocket, SD_SEND);
+
+            if (iResult == SOCKET_ERROR)
+            {
+                printf("shutdown failed: %d\n", WSAGetLastError());
+                close_connection();
+                return 1;
+            }
+
+            close_connection();
+            return LOGIN_FAIL;
+        }
+    }
+    else if (REGISTER_MODE)
+    {
+
+        // invia query di register al server
+        strcpy(qbuffer, "Register");
+
+        iResult = send(ConnectSocket, qbuffer, DEFAULT_BUFLEN, 0);
+
+        if (iResult == SOCKET_ERROR)
+        {
+            printf("send failed: %d\n", WSAGetLastError());
+            close_connection();
+            return 1;
+        }
+
+        // ricevi risultato register e ritorna valore corretto
+        iResult = recv(ConnectSocket, recvbuf, DEFAULT_BUFLEN, 0);
+
+        if (strcmp(recvbuf, "regok") == 0)
+        {
+            printf("register successful\n");
+
+            iResult = shutdown(ConnectSocket, SD_SEND);
+
+            if (iResult == SOCKET_ERROR)
+            {
+                printf("shutdown failed: %d\n", WSAGetLastError());
+                close_connection();
+                return 1;
+            }
+
+            return REGISTER_SUCCESS;
+        }
+
+        close_connection();
+        return REGISTER_FAIL;
+
+
+    }
+}
+
+// aggiorna il valore del saldo del giocatore sul database
+// modify_value è il valore che indica di quanto deve essere modificato il saldo
+// ritorna 0 in caso di successo, 1 in caso di errore
+int money_value_update(int modify_value)
+{
+
+    init_result = client_connection_init();
+
+    if (init_result == 1)
+    {
+        return 1;
+    }
+
+    // invia query di update al server
+    sprintf_s(qbuffer, DEFAULT_NUFLEN, "Modify:%d", modify_value);
+
+    iResult = send(ConnectSocket, qbuffer, DEFAULT_BUFLEN, 0);
+
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("send failed: %d\n", WSAGetLastError());
+
+        close_connection();
+        return 1;
+    }
+
+    // controlla se l'update ha avuto successo e ritorna il risultato dell'operazione
+    iResult = recv(ConnectSocket, recvbuf, DEFAULT_BUFLEN, 0);
+
+    if (iResult < 0)
+    {
+        printf("recv failed: %d\n", WSAGetLastError());
+        close_connection();
+        return 1;
+    }
+    else if (iResult == 0)
+    {
+        printf("server connection closed without warning\n");
+        close_connection();
+        return 1;
+    }
+
+    close_connection();
+    return 0;
+}
+
+// ritorna il valore del saldo salvato sul server
+// in caso di errore ritorna 1
+// in caso di valore non valido sul database ritorna 0
+int money_value_get()
+{
+    init_result = client_connection_init();
+    if (init_result == 1)
+    {
+        return 1;
+    }
+
+    // invia query per ricevere valore al server
+    strcpy(qbuffer, "GetVal");
+
+    iResult = send(ConnectSocket, qbuffer, DEFAULT_BUFLEN, 0);
+
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("send failed: %d\n", WSAGetLastError());
+        close_connection();
+        return 1;
+    }
+
+    // ritorna il valore del saldo
+    iResult = recv(ConnectSocket, recvbuf, DEFAULT_BUFLEN, 0);
+
+    if (strmp(recvbuf, "") != 0)
+    {
+        int return_value = (int)strtol(recvbuf, recvbuf[strlen(recvbuf)], 10);
+        close_connection();
+        return return_value;
+    }
+
+    printf("failure in receiving money amount\n");
+    close_connection();
+    return 0;
 }
 
