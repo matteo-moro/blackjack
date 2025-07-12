87e69f9d38824bf1e65c99de4ff0c791624bf603
matteo-moro <moro.matteorosario@gmail.com>
2025-07-12 20:01:24 +0200

    Update cardfunctions.c

--- client/cardfunctions.c
+++ client/cardfunctions.c
@@ -68,3 +68,71 @@ DECK fill_deck(int deck_number)
     return created_deck;
                     
 }
+
+// mischia un mazzo passato tramite puntatore attraverso lo shuffle di Fisher-Yates
+void shuffle_deck(DECK* to_shuffle)
+{
+    stand(time(NULL));
+
+    int card_number = to_shuffle->current_card_number;
+
+    for (int i = (card_number - 1); i > 0; i--)
+    {
+        int j = rand() % (i+1);
+        card_swap(&to_shuffle->cards[i], &to_shuffle->cards[j]);
+    }
+}
+
+// funzione util per scambiare carte
+void card_swap(CARD* first, CARD* second)
+{
+    CARD temp = *first:
+    *first = *second;
+    *second = temp;
+}
+
+// pesca una carta dal mazzo dato e la ritorna
+CARD draw_card(DECK* deck_to_use)
+{
+    CARD card_to_return = { 0 };
+
+    card_to_return.rank = deck_to_use->cards[0].rank;
+    card_to_return.suit = deck_to_use->cards[0].suit;
+
+    int card_number = deck_to_use->current_card_number;
+
+    for (int i = 0; i < card_number - 1; i++)
+    {
+        deck_to_use->cards[i].rank = deck_to_use->cards[i + 1].rank;
+        deck_to_use->cards[i].suit = deck_to_use->cards[i + 1].suit;
+    }
+
+    return card_to_return;
+}
+
+#ifdef GAME_BLACKJACK
+
+int bj_calculate_score(CARD hand_to_evaluate[], int card_number)
+{
+    int score_to_return = 0;
+
+    for (int i = 0; i < card_number; i++)
+    {
+        // visto che gli assi possono essere 11 o 1, sono prima trattati come 11 e poi eventualmente portati ad 1
+        if (hand_to_evaluate[i].rank == 1)
+        {
+            score_to_return += 11;
+        }
+        //le figure valgono sempre 10
+        else if (hand_to_evaluate[i].rank > 10)
+        {
+            score_to_return += 10;
+        }
+        else
+        {
+            score_to_return += hand_to_evaluate[i].rank;
+        }
+    }
+}
+
+#endif
